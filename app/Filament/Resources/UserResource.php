96d056f26f3a41676d7a276dde61fd91ac85133f
TheSoulrester <alex.gropp@icloud.com>
2024-10-03 21:10:00 +0200

    First Setup Laravel Filament with User-Table

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Checkbox;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\IconColumn;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            TextInput::make('name')->required(),
+            TextInput::make('email')->email()->required(),
+            Checkbox::make('is_admin')->label('Admin?')->default(false), // Admin-Status bearbeiten
+            TextInput::make('password')->password()->required(), // Passwort
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->sortable(),
+                TextColumn::make('email')->sortable(),
+                IconColumn::make('is_admin')
+                    ->label('Admin')
+                    ->boolean() // Zeigt verschiedene Icons für true/false an
+                    ->trueIcon('heroicon-o-check-circle') // Icon für true
+                    ->falseIcon('heroicon-o-x-circle') // Icon für false
+                    ->trueColor('success') // Farbe für true
+                    ->falseColor('danger'), // Farbe für false
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
