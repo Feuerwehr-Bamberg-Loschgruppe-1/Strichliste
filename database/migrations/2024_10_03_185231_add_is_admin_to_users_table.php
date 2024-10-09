85c47a7b46fa2a4fd308ebf1bf247a21abeafa5b
gaborm98 <gabordesigns@gmail.com>
2024-10-10 01:00:41 +0200

    Refactor is_admin column migration

--- database/migrations/2024_10_03_185231_add_is_admin_to_users_table.php
+++ database/migrations/2024_10_03_185231_add_is_admin_to_users_table.php
@@ -6,23 +6,10 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('is_admin')->default(false); // Hinzufügen des is_admin-Feldes
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
         });
     }
 };
