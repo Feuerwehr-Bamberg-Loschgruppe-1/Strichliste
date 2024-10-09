1c2cc50f2dc9f343d3feb7db71bba4189daa2765
gaborm98 <gabordesigns@gmail.com>
2024-10-10 00:02:25 +0200

    Update codestyle and add custom transaction modal action to users table

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -4,17 +4,18 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
 use App\Models\User;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
-
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\ToggleButtons;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\IconColumn;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Split;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -51,7 +52,7 @@ class UserResource extends Resource
                                 // Prüfe, ob der eingeloggte Benutzer seine eigenen Daten bearbeitet
                                 return Auth::user()->id === $get('id'); // Hier wird die Benutzer-ID verglichen
                             })
-                            ->helperText(fn($get) => Auth::user()->id === $get('id') ? 'Du kannst deinen eigenen Admin-Status nicht ändern.' : null)
+                            ->helperText(fn ($get) => Auth::user()->id === $get('id') ? 'Du kannst deinen eigenen Admin-Status nicht ändern.' : null)
                             ->afterStateUpdated(function (callable $set, $state) {
                                 // Wenn der Benutzer zum Admin gemacht wird, setze das Passwortfeld auf erforderlich
                                 if ($state) {
@@ -65,16 +66,16 @@ class UserResource extends Resource
                             ->password()
                             ->label('Passwort')
                             ->revealable()
-                            ->dehydrated(fn($state) => filled($state)) // Speichert nur, wenn ein Passwort gesetzt wird
-                            ->hidden(fn(callable $get) => !$get('is_admin')) // Feld nur anzeigen, wenn is_admin == true
-                            ->dehydrateStateUsing(fn($state) => filled($state) ? Hash::make($state) : null), // Passwort hashen, wenn vorhanden
+                            ->dehydrated(fn ($state) => filled($state)) // Speichert nur, wenn ein Passwort gesetzt wird
+                            ->hidden(fn (callable $get) => ! $get('is_admin')) // Feld nur anzeigen, wenn is_admin == true
+                            ->dehydrateStateUsing(fn ($state) => filled($state) ? Hash::make($state) : null), // Passwort hashen, wenn vorhanden
                         TextInput::make('password_confirmation')
                             ->password()
                             ->label('Passwort bestätigen')
                             ->revealable()
                             ->same('password') // Muss dem Passwort entsprechen
                             ->dehydrated(false) // Nicht speichern
-                            ->hidden(fn(callable $get) => !$get('is_admin')), // Nur anzeigen, wenn is_admin == true
+                            ->hidden(fn (callable $get) => ! $get('is_admin')), // Nur anzeigen, wenn is_admin == true
                     ])
                     ->compact(),
             ])->from('2xl'),
@@ -108,6 +109,15 @@ class UserResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('transactions')
+                    ->color('default')
+                    ->icon('heroicon-o-banknotes')
+                    ->modal(true)
+                    ->modalContent(
+                        fn (User $record): View => view('components.user-transaction-modal', ['user' => $record])
+                    )
+                    ->modalWidth(MaxWidth::FiveExtraLarge)
+                    ->modalSubmitAction(false),
             ])
             ->bulkActions([
                 //Tables\Actions\BulkActionGroup::make([
