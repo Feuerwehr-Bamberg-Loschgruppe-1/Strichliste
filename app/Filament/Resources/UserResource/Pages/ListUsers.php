2d91a2683b87caafa3a5880865f867f81752d8a7
TheSoulrester <alex.gropp@icloud.com>
2024-10-14 15:24:38 +0200

    Added getTabs to UserResource in ListUsers

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -6,6 +6,9 @@ use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
+
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
@@ -15,5 +18,20 @@ class ListUsers extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make('All'),
+            'poisitiveBalance' => Tab::make('Guthaben')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('balance', '>', 0)),
+            'negativeBalance' => Tab::make('Schulden')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('balance', '<', 0)),
+        ];
+    }
+    public function getDefaultActiveTab(): string | int | null
+    {
+        return 'All';
     }
 }
