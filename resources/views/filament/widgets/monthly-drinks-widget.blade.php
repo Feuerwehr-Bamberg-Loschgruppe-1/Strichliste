c6672b0b78afcd0653fe77cfcdbd3c1fbb039ec9
TheSoulrester <alex.gropp@icloud.com>
2024-10-17 08:27:38 +0200

    Widget styling

--- resources/views/filament/widgets/monthly-drinks-widget.blade.php
+++ resources/views/filament/widgets/monthly-drinks-widget.blade.php
@@ -3,22 +3,23 @@
         <x-slot name="heading">
             Monatliche Getränke
         </x-slot>
+        <div class="flex flex-col gap-y-6">
+            <!-- Tab Navigation -->
+            <x-filament::tabs label="Getränke pro Monat" class="flex justify-center max-w-max">
+                @foreach ($availableYears as $year)
+                    <x-filament::tabs.item
+                        wire:click="setYear({{ $year }})"
+                        :active="$currentYear == $year"
+                    >
+                        {{ $year }}
+                    </x-filament::tabs.item>
+                @endforeach
+            </x-filament::tabs>
 
-        <!-- Tab Navigation -->
-        <x-filament::tabs label="Getränke pro Monat" class="flex justify-center max-w-max">
-            @foreach ($availableYears as $year)
-                <x-filament::tabs.item
-                    wire:click="setYear({{ $year }})"
-                    :active="$currentYear == $year"
-                >
-                    {{ $year }}
-                </x-filament::tabs.item>
-            @endforeach
-        </x-filament::tabs>
-
-        <!-- Table Section -->
-        <div class="mt-4" style="margin-top: 20px;">
-            {{ $this->table }}
+            <!-- Table Section -->
+            <div>
+                {{ $this->table }}
+            </div>
         </div>
     </x-filament::section>
 </div>
