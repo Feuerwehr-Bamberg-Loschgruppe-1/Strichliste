6619faea2f3771fe474195ed297df4fb50abea10
TheSoulrester <alex.gropp@icloud.com>
2024-10-21 07:40:19 +0200

    fixed column width of monthly drinks widget

--- resources/views/filament/widgets/monthly-drinks-widget.blade.php
+++ resources/views/filament/widgets/monthly-drinks-widget.blade.php
@@ -1,23 +1,25 @@
-<x-filament::section class="monthly-drinks-widget">
-    <div class="flex flex-col gap-y-6">
-        <div class="flex items-center justify-between header-section">
-            <p class="flex-shrink-0 whitespace-nowrap title" style="margin-right: 1rem;">Monatliche Getränke</p>
-            <!-- Tab Navigation -->
-            <x-filament::tabs label="Getränke pro Monat" class="justify-end tabs-navigation">
-                @foreach ($availableYears as $year)
-                    <x-filament::tabs.item
-                        wire:click="setYear({{ $year }})"
-                        :active="$currentYear == $year"
-                        class="tab-item-{{ $year }}"
-                    >
-                        {{ $year }}
-                    </x-filament::tabs.item>
-                @endforeach
-            </x-filament::tabs>
+<x-filament-widgets::widget>
+    <x-filament::section class="monthly-drinks-widget">
+        <div class="flex flex-col gap-y-6">
+            <div class="flex items-center justify-between header-section">
+                <p class="flex-shrink-0 whitespace-nowrap title" style="margin-right: 1rem;">Monatliche Getränke</p>
+                <!-- Tab Navigation -->
+                <x-filament::tabs label="Getränke pro Monat" class="justify-end tabs-navigation">
+                    @foreach ($availableYears as $year)
+                        <x-filament::tabs.item
+                            wire:click="setYear({{ $year }})"
+                            :active="$currentYear == $year"
+                            class="tab-item-{{ $year }}"
+                        >
+                            {{ $year }}
+                        </x-filament::tabs.item>
+                    @endforeach
+                </x-filament::tabs>
+            </div>
+            <!-- Table Section -->
+            <div class="table-section" style="margin-left: -1.5rem; margin-right: -1.5rem; margin-bottom: -1.5rem;">
+                {{ $this->table }}
+            </div>
         </div>
-        <!-- Table Section -->
-        <div class="table-section" style="margin-left: -1.5rem; margin-right: -1.5rem; margin-bottom: -1.5rem;">
-            {{ $this->table }}
-        </div>
-    </div>
-</x-filament::section>
+    </x-filament::section>
+</x-filament-widgets::widget>
